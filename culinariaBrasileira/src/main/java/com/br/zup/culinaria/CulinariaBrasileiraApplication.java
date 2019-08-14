5a0e4856c919419e92e18abd05d8581ef106ad95
Diego2207 <diego.santos@zup.com.br>
2019-08-14 15:48:35 -0300

    adicionar metodo de criar receitas

---
+++ culinariaBrasileira/src/main/java/com/br/zup/culinaria/CulinariaBrasileiraApplication.java
@@ -0,0 +1,13 @@
+package com.br.zup.culinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinariaBrasileiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulinariaBrasileiraApplication.class, args);
+	}
+
+}
---
+++ culinariaBrasileira/src/main/java/com/br/zup/culinaria/repositories/ReceitaRepository.java
@@ -0,0 +1,9 @@
+package com.br.zup.culinaria.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.br.zup.culinaria.models.ReceitaModel;
+
+public interface ReceitaRepository  extends CrudRepository<ReceitaModel, Integer>{
+
+}
